075c01b0921d0e988f147079bb27c79d2ddebe2e
Panupong <Pai28212m@gmail.com>
2023-11-30 13:49:09 +0700

    HH:Pai

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,12 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+
+let message =
+  lightBulbStatus == "On"
+    ? "Light bulb is On."
+    : lightBulbStatus == "Off"
+    ? "lLight bulb is Off."
+    : "Please choose the correct input (On/Off)";
+
+console.log(message);
